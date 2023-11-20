c80d4bf0cf108f526f1deb2a1eb5de8841bb4b39
Aidan Duong <aidan.duong@gmx.de>
2023-11-20 17:09:11 +0100

    test initial commits

--- DataTypesTest.py
+++ DataTypesTest.py
@@ -1,3 +1,5 @@
+import random
+
 x = "Aidan"
 
 print(type(x))
@@ -5,3 +7,34 @@ print(isinstance(x ,str))
 
 dict = {'key1':'geeks', 'key2':'for'}
 print("Current Dict is: ", dict)
+
+
+
+def randomNumberGenerator(start, end):
+    randomResult = random.randrange(start, end, 10)
+    return randomResult
+
+
+g = randomNumberGenerator(0,100)
+print(g)
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+testString = "Separating the integer and decimal from the input number."
+
+stringSplit = testString.split(' ')
+
+print(testString)
+
+for x in stringSplit:
+    print (x)
+
