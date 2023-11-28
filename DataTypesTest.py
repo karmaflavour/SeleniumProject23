7436532e323018c8bd9f21c0ed25a816ef4b9d69
Aidan Duong <aidan.duong@gmx.de>
2023-11-28 02:41:44 +0100

    reformat changes commits

--- DataTypesTest.py
+++ DataTypesTest.py
@@ -3,11 +3,10 @@ import random
 x = "Aidan"
 
 print(type(x))
-print(isinstance(x ,str))
+print(isinstance(x, str))
 
-dict = {'key1':'geeks', 'key2':'for'}
+dict = {'key1': 'geeks', 'key2': 'for'}
 print("Current Dict is: ", dict)
-
 
 
 def randomNumberGenerator(start, end):
@@ -15,16 +14,18 @@ def randomNumberGenerator(start, end):
     return randomResult
 
 
-g = randomNumberGenerator(0,100)
+g = randomNumberGenerator(0, 100)
 print(g)
 
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
 
-  def myfunc(self):
-    print("Hello my name is " + self.name)
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+
 
 p1 = Person("John", 36)
 p1.myfunc()
@@ -36,5 +37,39 @@ stringSplit = testString.split(' ')
 print(testString)
 
 for x in stringSplit:
-    print (x)
+    print(x)
 
+d1 = {
+    "Name": ["Aidan", "Johnny", "Depp"],
+    "Age": 30
+
+}
+
+d2 = {
+
+    "location": "Germany",
+    "fruits:": {
+        "Name": ["Apple", "Banana", "Orange"], "Price": 100}
+}
+
+print("--------")
+
+print(d1)
+
+print("--------")
+
+for c in d1.items():
+    print(c)
+
+print("--------")
+
+d2 = {
+
+    "location": "Germany",
+    "fruits": {
+        "Name": ["Apple", "Banana", "Orange"], "Price": 100}
+}
+
+# print(d2["fruits"]["Name"][1])
+
+print(d2.get("fruits").get("Name"))
